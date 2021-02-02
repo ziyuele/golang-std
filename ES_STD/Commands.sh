cf73e6e5b43f790e4974dedc59910c102efef7c1
kangjian11 <kangjian11@meituan.com>
2021-02-02 23:05:24 +0800

    save

--- ES_STD/Commands.sh
+++ ES_STD/Commands.sh
@@ -1,7 +1,6 @@
 # create index
 CURRENT_PATH=$PWD
-curl s -H 'Content-Type:application/json' -X PUT ziyuele.com/test_index_1 -d@ES_STD/add_index.json
+curl -s -H 'Content-Type:application/json' -X PUT ziyuele.com/test_index_1 -d@ES_STD/add_index.json | python -m json.tool
 
 # getIndex
-
-curl s ziyuele.com/test_index_1
+curl -s ziyuele.com/test_index_1 | python -m json.tool
